92d2f0f33222cf7a3e92ab7854cb942d23191227
daimori <anraku90@gmail.com>
2019-12-29 11:43:38 +0900

    modify to output error

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
-	"golang.org/x/xerrors"
-
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 type m map[string]interface{}
@@ -13,8 +13,7 @@ type m map[string]interface{}
 func handler(c echo.Context) error {
 	err := usecase("hoge")
 	if err != nil {
-		fmt.Printf("%+v\n", err)
-		return c.JSON(500, m{"message": "App Errpr"})
+		return err
 	}
 	return c.JSON(200, m{"message": "success"})
 }
@@ -22,34 +21,81 @@ func handler(c echo.Context) error {
 func usecase(s string) error {
 	err := service(s)
 	if err != nil {
-		return xerrors.Errorf("[ERROR] usecase error: %w", err)
+		return ErrSomething.Wrap(err)
 	}
 	return nil
 }
 
 func service(s string) error {
-	return xerrors.New("occur error")
+	return errors.New("occur error")
 }
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(ErrorHandler)
 	e.GET("/", handler)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-type AppError struct {
-	Level   string
+func ErrorHandler(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var appErr *AppErr
+		if err := next(c); err != nil {
+			if errors.As(err, &appErr) {
+				switch appErr.Level {
+				case Fatal:
+					fmt.Printf("[%s] %d %+v\n", appErr.Level, appErr.Code, appErr.Unwrap())
+				case Error:
+					fmt.Printf("[%s] %d %+v\n", appErr.Level, appErr.Code, appErr.Unwrap())
+				case Warning:
+				}
+			} else {
+				appErr = ErrUnknown
+			}
+			c.JSON(appErr.Code, m{"message": appErr.Message})
+			return appErr.Unwrap()
+		}
+		return nil
+	}
+}
+
+type AppErr struct {
+	Level   ErrLevel
 	Code    int
 	Message string
-	Err     error
+	err     error
 }
 
-func (e *AppError) Error() string {
-	return e.Message
+func (e *AppErr) Error() string {
+	return fmt.Sprintf("[%s]: %d %s", e.Level, e.Code, e.Message)
 }
 
-var ErrSomething = &AppError{
-	Level:   "Fatal",
+type ErrLevel string
+
+const (
+	Fatal   ErrLevel = "FATAL"
+	Error   ErrLevel = "ERROR"
+	Warning ErrLevel = "WARNING"
+)
+
+var ErrSomething = &AppErr{
+	Level:   Error,
 	Code:    500,
 	Message: "something error",
+}
+
+var ErrUnknown = &AppErr{
+	Level:   Fatal,
+	Code:    500,
+	Message: "unknown error",
+}
+
+func (e *AppErr) Wrap(err error) error {
+	e.err = err
+	return e
+}
+
+func (e *AppErr) Unwrap() error {
+	return e.err
 }
